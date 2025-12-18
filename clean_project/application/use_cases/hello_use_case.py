81ccdbe83e25019bb021191a555a495f0a38328e
ageuribeiro <ageu87@gmail.com>
2025-12-18 07:46:36 -0300

    Delete dotnet-desktop.yml workflow

--- clean_project/application/use_cases/hello_use_case.py
+++
@@ -1,3 +0,0 @@
-class HelloUseCase:
-    def execute(self) -> str:
-        return "Hello World from Clean Architecture!"
--- clean_project/interface/cli/commands/hello.py
+++
@@ -1,7 +0,0 @@
-from application.use_cases.hello_use_case import HelloUseCase
-
-def hello_command():
-    use_case = HelloUseCase()
-    message = use_case.execute()
-    print(message)
-    
